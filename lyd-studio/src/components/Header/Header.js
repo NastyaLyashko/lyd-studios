24ea350b3ba9f7f4b8b15d9fb3eb5cce9e7a19ed
Nastya Lyashko <nastyalyashko@MacBook-Air-Nastya.local>
2021-05-13 21:16:52 +0300

    add contacts

--- lyd-studio/src/components/Header/Header.js
+++ lyd-studio/src/components/Header/Header.js
@@ -2,6 +2,9 @@ import './Header.css';
 import globus from '../../images/globus.svg';
 import book from '../../images/book.svg';
 import pc from '../../images/pc.svg';
+import telegram from '../../images/telegram.svg';
+import email from '../../images/email.svg';
+import phone from '../../images/phone.svg';
 
 function Header () {
     return(
@@ -19,7 +22,19 @@ function Header () {
                     <img className='header__logo' ></img>
                 </div>
                 <h2 className='header__subtitle' >ебанись, какой пафосный слоган!</h2>
+                <div className='header__contacts' >
+                    <a href="https://telegram.im/@alexoswart" target="_blank">
+                        <img className='header__contacts-logo' src={telegram} alt=''  />
+                    </a>
+                    <a href="mailto:someone@example.com">
+                    <img className='header__contacts-logo' src={email} alt=''  />
+                    </a>
+                    <a href="https://api.whatsapp.com/send?phone=79111274967">
+                    <img className='header__contacts-logo' src={phone} alt=''  />
+                    </a>
+                </div>
             </div>
+
         </header>
     )
 }
